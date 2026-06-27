012c6b57f26ef96ec2b3274cf29f7ec48139e5b4
dani3l0 <daniel.jank@protonmail.com>
2026-06-27 16:07:17 +0200

    Admin Page: Working services setup 2/2

---
+++ src/Admin/Pages/Apps/Property.svelte
@@ -0,0 +1,53 @@
+<script>
+	let { icon, name, value = $bindable(""), detectChanges } = $props()
+
+	const oninput = () => {
+		detectChanges()
+	}
+</script>
+
+<div class="property">
+	<div class="icon">
+		<span class="material-symbols-rounded">{icon}</span>
+	</div>
+	<div {oninput} data-placeholder={!value.length ? name : ""} class="value" contenteditable bind:textContent={value}></div>
+</div>
+
+<style>
+	.property {
+		display: flex;
+		align-items: stretch;
+		border: 1px solid #8886;
+		overflow: hidden;
+		border-radius: 20px;
+		flex: 1;
+		min-width: 240px;
+		position: relative;
+	}
+	.value {
+		padding: 12px 12px 12px 8px;
+		white-space: nowrap;
+		overflow-x: auto;
+		flex: 1;
+		transition: all .2s;
+	}
+	.value:focus {
+		outline: none;
+		background: #8883;
+	}
+	.value::before {
+		content: attr(data-placeholder);
+		color: #888B;
+		pointer-events: none;
+	}
+	.icon {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 40px;
+		padding-top: 1px;
+		padding-left: 4px;
+		margin-right: -8px;
+		flex-shrink: 0;
+	}
+</style>
---
+++ src/Admin/Pages/Client/Client.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { className } from "../../../App/engine/utils";
+    import Header from "../components/Header.svelte";
+
+	let { visible } = $props()
+</script>
+
+<div class="{className(visible, "visible")}">
+	<Header icon="display_settings" title="Client" color={90} />
+</div>
