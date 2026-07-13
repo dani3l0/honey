6d48d9decc5e0a5f1cc0a6355e87fabb8bdc32f2
dani3l0 <daniel.jank@protonmail.com>
2026-07-13 14:24:38 +0200

    Admin: personalization is now functional

--- src/Admin/Pages/Personalization/Personalization.svelte
+++ src/Admin/Pages/Personalization/Personalization.svelte
@@ -1,9 +1,42 @@
 <script>
 	import Header from "../../components/Header.svelte";
+    import Input from "../../components/Input.svelte";
+    import { configMain } from "../../engine/variables";
 
 </script>
 
 
 <div>
 	<Header icon="format_paint" title="Personalization" color={330} />
+
+	<Input
+		icon="match_case"
+		name="Name"
+		description="That big app name text on main screen"
+		bind:value={$configMain.personalization.name}
+	/>
+	<Input
+		icon="short_text"
+		name="Description"
+		description="Small, short text under the big app name"
+		bind:value={$configMain.personalization.description}
+	/>
+	<Input
+		icon="photo"
+		name="Favicon"
+		description="Icon that shows up on main screen and on your browser tab"
+		bind:value={$configMain.personalization.favicon}
+	/>
+	<Input
+		icon="wall_art"
+		name="Background image"
+		description="Wallpaper visible on light mode"
+		bind:value={$configMain.personalization.background_image}
+	/>
+	<Input
+		icon="wall_art"
+		name="Dark background image"
+		description="Wallpaper visible on dark mode"
+		bind:value={$configMain.personalization.background_image_dark}
+	/>
 </div>
---
+++ src/Admin/components/Input.svelte
@@ -0,0 +1,58 @@
+<script>
+	let { icon, name, type = "text", description, placeholder = "Empty", value = $bindable() } = $props()
+</script>
+
+
+<div class="config-raw">
+	<div class="info">
+		<div class="icon">
+			<span class="material-symbols-outlined">{icon}</span>
+		</div>
+		<div class="text">
+			<div class="name">{name}</div>
+			<div class="description">{description}</div>
+		</div>
+	</div>
+	<div class="input">
+		<input {type} {placeholder} bind:value>
+	</div>
+</div>
+
+
+<style>
+	.config-raw {
+		display: flex;
+		flex-direction: column;
+		background: #8883;
+		border-radius: 12px;
+		margin: 12px 8px;
+		overflow: hidden;
+	}
+	.info {
+		display: flex;
+		padding: 16px 6px 12px;
+		align-items: stretch;
+	}
+	.icon {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding: 0 12px;
+	}
+	.icon span {
+		font-size: 28px;
+	}
+	.description {
+		opacity: .5;
+		font-size: 0.9rem;
+	}
+	input {
+		background: #8882;
+		padding: 10px 14px;
+		font-size: 0.9rem;
+		transition: all .2s;
+	}
+	input:focus {
+		background: #8883;
+	}
+</style>
